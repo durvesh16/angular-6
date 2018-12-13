2253fee4e8aa3c2b4d562af4a7132c2311b53c56
durveshd <danidurvesh@gmail.com>
2018-12-13 23:00:28 +0530

    reactive form

--- src/app/thumbnail-form/thumbnail-form.component.ts
+++ src/app/thumbnail-form/thumbnail-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-thumbnail-form',
@@ -7,13 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThumbnailFormComponent implements OnInit {
 
+ 
+
+ 
+  constructor() { }
+  form;
+  ngOnInit() {
+    this.form = new FormGroup({
+     
+      medium: new FormControl(""),
+      name: new FormControl(""),
+      category: new FormControl(""),
+      year: new FormControl("")
+
+    });
+   
+  }
+
   onSubmit(thumbnailItem){
     console.log(thumbnailItem);
   }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
